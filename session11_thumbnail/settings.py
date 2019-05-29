500f9577e22d4ee719a0dc6a66bf2fae5c4a3fd3
leejieuns2 <2jjingni@gmail.com>
2019-05-29 20:33:46 +0900

    thumbnail commit

--- session11_thumbnail/settings.py
+++ session11_thumbnail/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # thumbnail
+    'imagekit',
     'thumbapp.apps.ThumbappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
